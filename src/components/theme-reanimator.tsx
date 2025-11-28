39f5e873561cd8f8cca119750ce70012c08395d1
AAFI MALEK <aafimalek2032@gmail.com>
2025-11-29 00:18:21 +0530

    reanimation

--- src/components/theme-reanimator.tsx
+++ src/components/theme-reanimator.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { AnimatePresence, motion } from "framer-motion";
 import { useTheme } from "next-themes";
 import { useEffect, useState } from "react";
 
@@ -15,5 +16,17 @@ export const ThemeReanimator = ({ children }: { children: React.ReactNode }) => 
         return <>{children}</>;
     }
 
-    return <div>{children}</div>;
+    return (
+        <AnimatePresence mode="wait">
+            <motion.div
+                key={theme}
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                exit={{ opacity: 0 }}
+                transition={{ duration: 0.2 }}
+            >
+                {children}
+            </motion.div>
+        </AnimatePresence>
+    );
 };
